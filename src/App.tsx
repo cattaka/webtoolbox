8740f758cadc27d513045808a8a66de4fc5eb6b7
Takao Sumitomo <cattaka@mail.cattaka.net>
2020-02-09 22:28:55 +0900

    Enable firebase analytics

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,10 @@ import Base64 from './pages/base64'
 import DataUri from './pages/data-uri'
 import About from './pages/about'
 import Page from "./components/Page";
+import firebase from "firebase";
+import { firebase_settings } from "./settings";
+
+firebase.initializeApp(firebase_settings);
 
 const App: React.FC = () => {
   return (
@@ -56,31 +60,31 @@ const App: React.FC = () => {
         <div className={'App-content'}>
           <Switch>
             <Route path="/diff-csv">
-              <Page title={'Diff CSV - WebToolbox'}><DiffCsv /></Page>
+              <Page title={'Diff CSV - WebToolbox'} trackName={'DiffCsv'}><DiffCsv /></Page>
             </Route>
             <Route path="/format-json">
-              <Page title={'Format JSON - WebToolbox'}><FormatJson /></Page>
+              <Page title={'Format JSON - WebToolbox'} trackName={'FormatJson'}><FormatJson /></Page>
             </Route>
             <Route path="/pull-by-regex">
-              <Page title={'Pull by regex - WebToolbox'}><PullByRegex /></Page>
+              <Page title={'Pull by regex - WebToolbox'} trackName={'PullByRegex'}><PullByRegex /></Page>
             </Route>
             <Route path="/force-directed-graph">
-              <Page title={'Force directed graph - WebToolbox'}><ForceDirectedGraph /></Page>
+              <Page title={'Force directed graph - WebToolbox'} trackName={'ForceDirectedGraph'}><ForceDirectedGraph /></Page>
             </Route>
             <Route path="/urlencode">
-              <Page title={'Url encode - WebToolbox'}><UrlEncode /></Page>
+              <Page title={'Url encode - WebToolbox'} trackName={'UrlEncode'}><UrlEncode /></Page>
             </Route>
             <Route path="/base64">
-              <Page title={'Base64 - WebToolbox'}><Base64 /></Page>
+              <Page title={'Base64 - WebToolbox'} trackName={'Base64'}><Base64 /></Page>
             </Route>
             <Route path="/data-uri">
-              <Page title={'Data URI - WebToolbox'}><DataUri /></Page>
+              <Page title={'Data URI - WebToolbox'} trackName={'DataUri'}><DataUri /></Page>
             </Route>
             <Route path="/about">
-              <Page title={'About - WebToolbox'}><About /></Page>
+              <Page title={'About - WebToolbox'} trackName={'About'}><About /></Page>
             </Route>
             <Route path="/">
-              <Page title={'WebToolbox'}><Home /></Page>
+              <Page title={'WebToolbox'} trackName={'Home'}><Home /></Page>
             </Route>
           </Switch>
         </div>
